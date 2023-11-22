5cd62aa5e478d4afadcf710bde4c79a905d96339
Harsh Rai <harshpayal3252@gmail.com>
2023-11-22 14:59:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://harsh3252:1234@deploylearn.sc6x3wh.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a user schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String
+});
+
+// Define a user model
+const User = mongoose.model('User', userSchema);
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
